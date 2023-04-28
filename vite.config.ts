49fa86a7f14aaed3a849ad7436c2125fb50b9c15
LilleAila <olai.solsvik@gmail.com>
2023-04-28 14:01:39 +0200

    New favicon and fixed links

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/skyblock-items-search/",
   plugins: [react()],
 })
